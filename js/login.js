e52ad52a8acaf5e1c57471fdde774e8fae57729d
iaraechenique <iaraechenique0816@gmail.com>
2024-08-19 16:54:35 -0300

    final login.js

--- js/login.js
+++ js/login.js
@@ -7,20 +7,12 @@ button.addEventListener('click', (event) => {
     const usuario = document.getElementById('user').value; //contante de los campos
     const password = document.getElementById('pass').value;
 
-    if (!usuario || !password) {  //condicion para redirigir
-        alert("Los campos deben estar llenos");
-    } else {
-        saveLogin(usuario, password);
+    if (usuario && password) {
+        sessionStorage.setItem('authenticatedUser', usuario);
+
+        // Redirige al usuario a la página principal
         window.location.href = "index.html";
+    } else {
+        alert("Los campos deben estar llenos");
     }
 });
-
-let saveLogin = function (usuario, password) {
-    const userkey = "user_key";
-    const passwordkey = "password_key";
-
-    localStorage.setItem(userkey, usuario);
-    localStorage.setItem(passwordkey, password);
-}
-
-
