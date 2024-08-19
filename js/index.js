55b53b7fd56910fba30f348cc3cdd0e3f6f85b0e
iaraechenique <iaraechenique0816@gmail.com>
2024-08-19 16:55:55 -0300

    final index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    if (checkLogin()) {
-        document.getElementById("autos").addEventListener("click", function () {
-            localStorage.setItem("catID", 101);
-            window.location = "products.html"
-        });
-        document.getElementById("juguetes").addEventListener("click", function () {
-            localStorage.setItem("catID", 102);
-            window.location = "products.html"
-        });
-        document.getElementById("muebles").addEventListener("click", function () {
-            localStorage.setItem("catID", 103);
-            window.location = "products.html"
-        });
+    // Verifica la autenticación al cargar la página
+    if (!checkAuthentication()) {
+        // Redirige a la página de inicio de sesión si el usuario no está autenticado
+        window.location.href = "login.html";
     } else {
-        window.location = "login.html"
+        // Configura los eventos de los botones solo si el usuario está autenticado
+        setupEventListeners();
     }
-
 });
 
-let checkLogin = function(){
-    const userkey = "user_key";
-    const passwordkey = "password_key";
-
-    const user = localStorage.getItem(userkey);
-    const password = localStorage.getItem(passwordkey);
-
-    const isLogged = user !== null && password !== null;
-    return isLogged;
+// Función para verificar la autenticación
+function checkAuthentication() {
+    const authUser = sessionStorage.getItem('authenticatedUser');
+    console.log('Authenticated User:', authUser); // Para depurar
+    return authUser !== null;
 }
+
+// Configura los eventos de los botones
+function setupEventListeners() {
+    document.getElementById("autos").addEventListener("click", function () {
+        localStorage.setItem("catID", 101);
+        window.location.href = "products.html";
+    });
+
+    document.getElementById("juguetes").addEventListener("click", function () {
+        localStorage.setItem("catID", 102);
+        window.location.href = "products.html";
+    });
+
+    document.getElementById("muebles").addEventListener("click", function () {
+        localStorage.setItem("catID", 103);
+        window.location.href = "products.html";
+    });
+}
+
+});
 
