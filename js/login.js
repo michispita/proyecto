b1cf468e4ba71cf8384baea70caed0e7237077ca
iaraechenique <iaraechenique0816@gmail.com>
2024-08-19 14:49:01 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-
 const button = document.getElementById('but'); //creamos la constante del boton
 
 button.addEventListener('click', (event) => {
@@ -10,15 +9,12 @@ button.addEventListener('click', (event) => {
     if (!usuario || !password) {  //condicion para redirigir
         alert("Los campos deben estar llenos");
     } else {
+        saveLogin(usuario, password);
         window.location.href = "index.html";
     }
 });
 
-/* aquí, la funcion saveloguin (que en este caso es anonima), almacenará las credenciales de el usuario.
-En la funcion podemos ver que se definen dos constantes (userkey y passwordkey) que sirven como claves
-para almacenar valores en el almacenamiento local del navegador,el (localstorage)*/
-
-let saveLogin = function (usuario, password) {
+let saveLogin = function (usuario, password) {  // funcion saveLogin.
     const userkey = "user_key";
     const passwordkey = "password_key";
 
