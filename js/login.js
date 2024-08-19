82df4052678c58653e945af34dec47fda76f18d2
iaraechenique <iaraechenique0816@gmail.com>
2024-08-19 12:32:49 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -14,5 +14,16 @@ button.addEventListener('click', (event) => {
     }
 });
 
+/* aquí, la funcion saveloguin (que en este caso es anonima), almacenará las credenciales de el usuario.
+En la funcion podemos ver que se definen dos constantes (userkey y passwordkey) que sirven como claves
+para almacenar valores en el almacenamiento local del navegador,el (localstorage)*/
+
+let saveLogin = function (usuario, password) {
+    const userkey = "user_key";
+    const passwordkey = "password_key";
+
+    localStorage.setItem(userkey, usuario);
+    localStorage.setItem(passwordkey, password);
+}
 
 
