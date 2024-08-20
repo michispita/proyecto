ed0c53ad70ee15b57d9459af3512c0df4b48c41e
iaraechenique <iaraechenique0816@gmail.com>
2024-08-20 19:38:38 -0300

    cambio final  index.js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Verifica la autenticación al cargar la página
-    if (!checkAuthentication()) {
-        // Redirige a la página de inicio de sesión si el usuario no está autenticado
-        window.location.href = "login.html";
+    if (checkLogin()) {
+        document.getElementById("autos").addEventListener("click", function () {
+            localStorage.setItem("catID", 101);
+            window.location = "products.html"
+        });
+        document.getElementById("juguetes").addEventListener("click", function () {
+            localStorage.setItem("catID", 102);
+            window.location = "products.html"
+        });
+        document.getElementById("muebles").addEventListener("click", function () {
+            localStorage.setItem("catID", 103);
+            window.location = "products.html"
+        });
     } else {
-        // Configura los eventos de los botones solo si el usuario está autenticado
-        setupEventListeners();
+        window.location = "login.html"
     }
-});
-
-// Función para verificar la autenticación
-function checkAuthentication() {
-    const authUser = sessionStorage.getItem('authenticatedUser');
-    return authUser !== null;
-}
-
-// Configura los eventos de los botones
-function setupEventListeners() {
-    document.getElementById("autos").addEventListener("click", function () {
-        localStorage.setItem("catID", 101);
-        window.location.href = "products.html";
-    });
-
-    document.getElementById("juguetes").addEventListener("click", function () {
-        localStorage.setItem("catID", 102);
-        window.location.href = "products.html";
-    });
-
-    document.getElementById("muebles").addEventListener("click", function () {
-        localStorage.setItem("catID", 103);
-        window.location.href = "products.html";
-    });
-}
 
 });
 
+let checkLogin = function(){
+    const userkey = "user_key";
+    const passwordkey = "password_key";
+
+    const user = localStorage.getItem(userkey);
+    const password = localStorage.getItem(passwordkey);
+
+    const isLogged = user !== null && password !== null;
+    return isLogged;
+}
