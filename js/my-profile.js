a921dbaca363c685513907caa7253de1bcabd6f9
Julieta <julietadiazh02@gmail.com>
2024-10-15 20:34:12 -0300

    agregue foto y nombre

---
+++ js/my-profile.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("nombre");
+const segNombre = document.getElementById("segNombre");
+const apellido = document.getElementById("apellido");
+const segApellido = document.getElementById("segApellido");
+const email = document.getElementById("email");
+const contacto = document.getElementById("contacto");
