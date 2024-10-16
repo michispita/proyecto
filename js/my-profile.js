4ae5a4df22d4c6d6881d88d75ee89f058aabb21d
Julieta <julietadiazh02@gmail.com>
2024-10-15 21:08:37 -0300

    required e intento de aplicar lo de bootstarp para validaciones

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,43 @@
+//script de validacion
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
 const nombre = document.getElementById("nombre");
 const segNombre = document.getElementById("segNombre");
 const apellido = document.getElementById("apellido");
 const segApellido = document.getElementById("segApellido");
 const email = document.getElementById("email");
 const contacto = document.getElementById("contacto");
+const modo = document.getElementsByClassName("mode");
+const btnGuardar = document.getElementsByClassName("userbtn");
+
+
+btnGuardar.addEventListener('submit', function (event) {
+    verificar () {
+    
+        if (!) {
+    
+          } else {
+            
+          }
+        };
+
+
+
+    });
