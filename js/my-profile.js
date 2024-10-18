5a44d2e7275ea534ef24162e9582b21c49b430ef
laura930 <174218186+laura930@users.noreply.github.com>
2024-10-17 21:32:29 -0300

    modo fail

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,20 +1,21 @@
 
 
 const btnGuardar= document.getElementsByClassName("userbtn"); // boton guarda cambios definidos en el perfil del usuario.
-const htmlProInfo= document.getElementById("proBody"); // agrege id en la etiqueta html del archivo de product-info.html
-const checkbox= document.getElementById("toggle");
+const htmlProInfo= document.getElementById("htmlProInfo"); // agrege id en la etiqueta html del archivo de product-info.html
+const htmlProfile= document.getElementById("htmlProfile");
+let checkbox= document.getElementById("checkbox");
 let modo;
 let darkmode= localStorage.setItem("mode","darkmode")
-const themeSwitch= document.getElementById("toggle")
-    
+const themeSwitch= document.getElementById("checkbox")
+   
 const enableDarkmode= () =>{
 // document.body.classList.add("darkmode")
-htmlProInfo.classList.add("darkmode")
+htmlProfile.classList.add("darkmode")
 localStorage.setItem("darkmode","active")
 }
 
 const disableDarkmode= () => {
-    htmlProInfo.classList.remove("darkmode")
+    htmlProfile.classList.remove("darkmode")
     localStorage.setItem("darkmode", null)
 }
 if(darkmode==="active") {
@@ -25,29 +26,30 @@ if(darkmode==="active") {
   darkmode!=="active"? enableDarkmode() : disableDarkmode() //zzz? xxx : xxy same sintax if(zzz){xxx} else{xxy}
  });
 
-/* Cargar la preferencia del usuario al iniciar
+/*Cargar la preferencia del usuario al iniciar
 document.addEventListener("DOMContentLoaded", () => {
     const userPref = localStorage.getItem("userPref");
     if (userPref) {
         htmlProInfo.setAttribute("data-bs-theme", userPref);
         checkbox.checked = userPref === "dark"; // Sincronizar el estado del checkbox
     }
-});
+}); */
 
-// Escuchar el cambio en el checkbox
-checkbox.addEventListener("change", function guardarCambios() {
-    if (checkbox.checked) {
-        modo = "dark";
+/*Escuchar el cambio en el checkbox
+checkbox.addEventListener("change", () => {
+    //alert("Entro");
+   if (checkbox.checked) {
+        htmlProfile.setAttribute("data-bs-theme","dark" );
     } else {
-        modo = "light";
+       htmlProfile.setAttribute("data-bs-theme","light" );
     }
-    
+   /* 
     // Cambiar el atributo del tema
     htmlProInfo.setAttribute("data-bs-theme", modo);
     
     // Guardar la preferencia en localStorage
     localStorage.setItem("userPref", modo);
-    console.log(localStorage);
-});
+    console.log(localStorage); 
+});*/
 
-*/
+
